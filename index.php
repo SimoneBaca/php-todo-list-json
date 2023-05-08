cdb6b9e71eaa1c66e2532a68c425712f802dbbd5
Simonebaca <simobaco99@gmail.com>
2023-05-08 19:02:39 +0200

    add new task to fix

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>PHP Todo List Json</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
+
 </head>
 
 <body>
@@ -16,27 +17,21 @@
         <div class="container">
             <div class="tasklist mx-5 p-3">
                 <ul class="list-group list-group-flush w-50 p-3 bg-white rounded-2 m-auto">
-                    <ul class="list-group list-group-flush w-50 p-3 bg-white rounded-2 m-auto">
-                        <li class="list-group-item">item1</li>
-                        <li class="list-group-item">item2</li>
-                        <li class="list-group-item">item3</li>
-                        <li class="list-group-item">item4</li>
-                        <li class="list-group-item">item5</li>
-                    </ul>
-
+                    <li class="list-group-item" v-for="task in tasks">{{task}}</li>
+                </ul>
             </div>
             <div class="addTask p-4">
-                <div class="input-group mb-3 w-50 m-auto">
-                    <input type="text" class="form-control" placeholder="New Task...">
-                    <button type="button" class="btn btn-primary">Add
+                <form class="input-group mb-3 w-50 m-auto">
+                    <input type="text" class="form-control" placeholder="New Task..." name="newTask" id="newTask" v-model="newTask">
+                    <button type="submit" class="btn btn-primary" @click="addTask">Add
                         Task</button>
-                </div>
+                </form>
             </div>
         </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.global.min.js"></script>
-        <script src="./app.js"></script>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./app.js"></script>
 
 </body>
 
--- server.php
+++ server.php
@@ -1,8 +1,16 @@
 <?php
+
 $tasks = file_get_contents('tasks.json');
+//var_dump($tasks);
+$taskList = json_decode($tasks);
 
-header('Content-Type: application/json');
-echo json_encode($tasks);
+if (isset($_POST['newTask'])) {
+    array_push($taskList, $_POST['newTask']);
+    $jsonList = json_encode($taskList);
+    //var_dump($tasks);
+    file_put_contents('tasks.json', $jsonList);
+}
 
 
- 
+header('Content-Type:application/json');
+echo json_encode($taskList);
