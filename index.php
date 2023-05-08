43af95519d60c81af7c19978d6f305638c2ba18b
Simonebaca <simobaco99@gmail.com>
2023-05-08 17:09:54 +0200

    Add little bit style

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
 
 <body>
     <div id="app">
-        <h1>Todo List</h1>
+        <h1 class="text-center p-3 mb-3">Todo List</h1>
         <div class="container">
-            <div>
-                <ul>
+            <div class="tasklist mx-5 p-3">
+                <ul class="list-group list-group-flush w-50 p-3 bg-white rounded-2 m-auto">
                     <li class="list-group-item">item1</li>
                     <li class="list-group-item"> item2</li>
                     <li class="list-group-item"> item3</li>
@@ -23,10 +23,10 @@
                     <li class="list-group-item">item5</li>
                 </ul>
             </div>
-            <div>
-                <div>
-                    <input type="text" placeholder="New Task...">
-                    <button type="button">Add
+            <div class="addTask p-4">
+                <div class="input-group mb-3 w-50 m-auto">
+                    <input type="text" class="form-control" placeholder="New Task...">
+                    <button type="button" class="btn btn-primary">Add
                         Task</button>
                 </div>
             </div>
